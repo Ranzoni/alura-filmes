97f2a04ad8c756ee7699b86d7a09eb54a1c860fd
Felipe Ranzoni Borges <felipe.ranzoni@hotmail.com>
2022-02-23 09:29:42 -0300

    first commit

---
+++ Alura.Filmes.App/Dados/AluraFilmesContexto.cs
@@ -0,0 +1,32 @@
+﻿using Alura.Filmes.App.Negocio;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+
+namespace Alura.Filmes.App.Dados
+{
+    public class AluraFilmesContexto : DbContext
+    {
+        public DbSet<Ator> Atores { get; set; }
+        public DbSet<Filme> Filmes { get; set; }
+        public DbSet<FilmeAtor> Elenco { get; set; }
+        public DbSet<Idioma> Idiomas { get; set; }
+        public DbSet<Cliente> Clientes { get; set; }
+        public DbSet<Funcionario> Funcionarios { get; internal set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=AluraFilmes;Trusted_Connection=true;");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration(new AtorConfiguration());
+            modelBuilder.ApplyConfiguration(new FilmeConfiguration());
+            modelBuilder.ApplyConfiguration(new FilmeAtorConfiguration());
+            modelBuilder.ApplyConfiguration(new IdiomaConfiguration());
+            modelBuilder.ApplyConfiguration(new ClienteConfiguration());
+            modelBuilder.ApplyConfiguration(new FuncionarioConfiguration());
+        }
+    }
+}
---
+++ Alura.Filmes.App/Program.cs
@@ -0,0 +1,25 @@
+﻿using Alura.Filmes.App.Dados;
+using Alura.Filmes.App.Extensions;
+using Alura.Filmes.App.Negocio;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Data.SqlClient;
+using System.Linq;
+
+namespace Alura.Filmes.App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var contexto = new AluraFilmesContexto())
+            {
+                contexto.LogSQLToConsole();
+
+                var sql = "select * from actor";
+                Console.WriteLine("Execução do SELECT...");
+                contexto.Database.ExecuteSqlCommand(sql);
+            }
+        }
+    }
+}
